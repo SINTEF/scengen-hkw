1f98d18b8f7c43ad535915757b52d5b195ea6351
Michal Kaut <michalkaut@gmail.com>
2011-01-27 16:23:49 +0100

    additional commit

---
+++ HKW_sg.h
@@ -0,0 +1,76 @@
+/*********************************************************************/
+/***  Høyland-Kaut-Wallace algorithm for scenario generation       ***/
+/***                                                               ***/
+/***  author: Michal Kaut                                          ***/
+/*********************************************************************/
+
+#ifndef HKW_SG_H
+#define HKW_SG_H
+
+#include "misc_macros.h" // defines DLL_PUBLIC
+#include "matrix.h"
+
+
+/// main scenario-generation procedure
+/**
+	Generates one subtree using the HKW algorithm (heuristics)
+	\param[in] FormatOfMoms - format of the target moments
+		It is a sum of following bits:
+		- 1 -> 2nd moment is Var instead of StDev
+		- 2 -> 4th moment is Kurtose - 3
+		- 4 -> Higher moments are not scaled by StDev
+		- 8 -> TarMom[i-1] = E{X^i} ... lower bits are ignored in this case \n
+		The reasonable values are 0,1,2,3,4,5,6,7,8
+	\param[in] p_TarMoms pointer to matrix of target moments
+	\param[in] p_TgCorrs pointer to matrix of target correlations
+	\param[in] p_Probs pointer to vector of target probabilities
+	\param     p_OutMat pointer to matrix where the scenarios go
+	\param[in] MaxErrMom maximum allowed error for moments
+	\param[in] MaxErrCorr maximum allowed error for correlations
+	\param[in] TestLevel level of output during the algorithm
+	\param[in] MaxTrial maximum number of trials
+	\param[in] MaxIter maximum number of iterations in each trial
+	\param[in] UseStartValues use values in p_OutMat as a starting point \n
+		This will only be used in the first trial. If this does not converge
+		and \a MaxTrial > 1, the other trials start from a random starting point.
+
+	The main structure of the algorithm is as follows:
+	- 1	trial = 1
+	- 2	if UseStartValues = true and trial = 1, use the current values in
+	   	\a p_OutMat \n otherwise generate a random starting point
+	- 3	iter = 1
+	- 4	if error-in-moments > \a MaxErrMom, correct moments
+	- 5	if error-in-correlations > \a MaxErrCorr, correct correlations
+	- 6	if one of the errors > max and iter < \a MaxIter,
+	   	then iter++ and go to 4
+	- 7	if one of the errors > max and trial < \a MaxTrial,
+	   	then trial++ and go to 2
+	- 8	if both errors < max, report the results (we have convergence),
+		otherwise report failure and return the best scenarios found
+**/
+DLL_PUBLIC int HKW_ScenGen(int const FormatOfMoms,
+                           TMatrix const * const p_TarMoms,
+                           TMatrix const * const p_TgCorrs,
+                           TVector const * const p_Probs,
+                           TMatrix * const p_OutMat,
+                           double const MaxErrMom, double const MaxErrCorr,
+                           int const TestLevel, int const MaxTrial,
+                           int const MaxIter, int const UseStartValues);
+
+
+/// wrapper for HKW_ScenGen(), using only standard C arrays
+/**
+	For description of parameters see HKW_ScenGen(). \n
+	The only difference, apart from the types, is that \a probs is allowed
+	to be NULL, meaning equiprobable scenarios, and \a outSc is allowed to
+	be NULL, in which case it gets allocated.
+**/
+DLL_PUBLIC int scengen_HKW(double ** const tgMoms, int const FormatOfMoms,
+                           double ** const tgCorrs, double * const probs,
+                           int const nVar, int const nScen,
+                           double ** const outSc,
+                           double const MaxErrMom, double const MaxErrCorr,
+                           int const TestLevel, int const MaxTrial,
+                           int const MaxIter, int const UseStartValues);
+
+#endif
---
+++ misc_macros.h
@@ -0,0 +1,89 @@
+#ifndef MISC_MACROS_H
+#define MISC_MACROS_H
+
+// ----------------------------------------------------------------
+// random-number generation macros
+
+/* Random number generator - double from [0,1]
+	 rand() is the only random number generator in C++
+	 Note that rand() is from 0 to RAND_MAX inclusive!
+	 */
+#define frand (((double) rand() + 0.5) / ((double) RAND_MAX + 1.0))
+
+/* Random number generator - integer from 0 to Max-1
+	 rand() is the only random number generator in VC++
+	 Note that rand() is from 0 to RAND_MAX inclusive!
+	 */
+#define irand(Max) (rand() % (Max))
+
+/* Random numbers from standard normal distribution
+	 using Box-Muller formula
+	 */
+#define Pi 3.1415926535
+#define BM_normal() (double)(sqrt(-2*log(frand))*cos(2*Pi*frand))
+
+
+// ----------------------------------------------------------------
+// gen. mathematical macros
+
+/* These definitions need GNU C extensions!
+   With the standard definitions (see below),
+   min(x,f(x)) will cause f(x) to be computed twice.
+   These definitions avoid the problem...
+   */
+#define min(X, Y)                     \
+({ typeof (X) __x = (X), __y = (Y);   \
+  (__x < __y) ? __x : __y; })
+#define max(X, Y)                     \
+({ typeof (X) __x = (X), __y = (Y);   \
+  (__x > __y) ? __x : __y; })
+/* Standard definitions:
+#define min(X, Y)  ((X) < (Y) ? (X) : (Y))
+#define max(X, Y)  ((X) > (Y) ? (X) : (Y))
+*/
+
+// use this for comparing two float numbers
+#define EPS 1e-8
+
+
+// ----------------------------------------------------------------
+// generic C/C++ macros
+
+#define allocate(var, type, size) if ((var = (type *) malloc(size * sizeof(type))) == NULL) { printf("\nNOT ENOUGH MEMORY!\n\n");	exit(1); }
+
+
+// ----------------------------------------------------------------
+// macros for cross-platform dll-exporting
+
+#ifndef DLL_EXPORT_DEF_H
+#define DLL_EXPORT_DEF_H
+
+#if defined _WIN32
+  #ifdef BUILDING_DLL
+    #ifdef __GNUC__
+      #define DLL_PUBLIC __attribute__((dllexport))
+    #else
+      #define DLL_PUBLIC __declspec(dllexport) // Note: actually gcc seems to also supports this syntax.
+    #endif
+  #else
+    #ifdef __GNUC__
+      #define DLL_PUBLIC __attribute__((dllimport))
+    #else
+      #define DLL_PUBLIC __declspec(dllimport) // Note: actually gcc seems to also supports this syntax.
+    #endif
+  #endif
+  #define DLL_LOCAL
+#else
+  #if __GNUC__ >= 4
+    #define DLL_PUBLIC __attribute__ ((visibility("default")))
+    #define DLL_LOCAL  __attribute__ ((visibility("hidden")))
+  #else
+    #define DLL_PUBLIC
+    #define DLL_LOCAL
+  #endif
+#endif
+
+#endif
+
+
+#endif
